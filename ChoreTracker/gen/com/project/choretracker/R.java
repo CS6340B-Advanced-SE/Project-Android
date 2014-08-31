715d59672b29bef13cc21d240bf5062dddde8547
josedeleon <jdeleon@stmarytx.edu>
2014-08-31 09:25:53 -0500

    Adding resource for authentication view

--- ChoreTracker/gen/com/project/choretracker/R.java
+++ ChoreTracker/gen/com/project/choretracker/R.java
@@ -32,9 +32,10 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int chore_tracker_home=0x7f030001;
-        public static final int chore_tracker_rewards=0x7f030002;
-        public static final int fragment_main=0x7f030003;
+        public static final int authentication_layout=0x7f030001;
+        public static final int chore_tracker_home=0x7f030002;
+        public static final int chore_tracker_rewards=0x7f030003;
+        public static final int fragment_main=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
